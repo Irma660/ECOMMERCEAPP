f9bc62bc2d2e5aa83d3b923f05b5e81b08561e78
irma660 <1325455@senati.pe>
2024-03-23 11:30:27 -0500

    save

--- app/src/main/java/com/example/ecommerce/RegistrationActivity.java
+++ app/src/main/java/com/example/ecommerce/RegistrationActivity.java
@@ -8,11 +8,15 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
@@ -65,8 +69,18 @@ public class RegistrationActivity extends AppCompatActivity {
             return;
            }
         auth.createUserWithEmailAndPassword(userEmail,userPassword)
-                        .addOnCompleteListener()
-        startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+                        .addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()){
+                                    Toast.makeText(RegistrationActivity.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+                                }else {
+                                    Toast.makeText(RegistrationActivity.this, "Registro fallido"+task.getException(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
     }
 
     public void singin(View view) {
