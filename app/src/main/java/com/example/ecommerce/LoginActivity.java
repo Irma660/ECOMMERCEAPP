6e70ba78aa8a4217a75faec9106e7e2146ee8f46
irma660 <1325455@senati.pe>
2024-03-23 09:39:08 -0500

    login

--- app/src/main/java/com/example/ecommerce/LoginActivity.java
+++ app/src/main/java/com/example/ecommerce/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.ecommerce;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,11 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void signIn(View view) {
+    }
+
+    public void singUp(View view) {
     }
 }
